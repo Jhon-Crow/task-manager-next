a66bb7191ed77aa8b5c044bc124152612b2c9448
Jhon-Crow <jhamish@mail.ru>
2025-05-08 03:18:30 +0300

    add delete & update (unchecked)

---
+++ src/entities/user/model/service/deleteUserById/deleteUserById.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { handleAction } from "@/shared/lib/actions";
+import { prisma } from "@/shared/lib/db/prisma";
+
+const deleteUserByIdImplementation = async (id: unknown) => {
+  if (typeof id !== "string") {
+    // TODO
+    throw new Error("deleteTask ERROR");
+  }
+  await prisma.user.delete({ where: { id } });
+};
+
+export const deleteUserById = async (id: unknown) =>
+  handleAction<void, unknown>(deleteUserByIdImplementation, id);
