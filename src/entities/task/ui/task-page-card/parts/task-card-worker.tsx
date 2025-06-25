426dca53dbcaed327b3dc6f985b46d0fc7f74ca5
Nikita Andryashin <yashiandr@gmail.com>
2025-06-25 15:33:49 +0330

    some fix task card worker

--- src/entities/task/ui/task-page-card/parts/task-card-worker.tsx
+++ src/entities/task/ui/task-page-card/parts/task-card-worker.tsx
@@ -3,7 +3,6 @@ import { UserAvatar } from "../../../../user";
 import { Routes } from "@/shared/routes/paths";
 import { cn } from "@/shared/lib/utils";
 import Link from "next/link";
-import {Tooltip, TooltipContent, TooltipTrigger} from "@/shared/ui";
 
 export const TaskCardWorker = ({
   worker,
@@ -12,38 +11,24 @@ export const TaskCardWorker = ({
   worker: TypeTaskWorker;
   position: "last" | "center" | "first";
 }) => (
-    <Tooltip key={worker.id}>
-        <TooltipTrigger asChild>
-            <Link
-                href={Routes.USER(worker.id)}
-                className={cn(
-                    "group flex items-center gap-x-2 hover:z-10 -mr-4 transition-all",
-                    {
-                        "hover:mr-0.5": position === "last",
-                        "hover:px-2 hover:mr-2": position === "center",
-                        "hover:pl-2.5 hover:pr-0.5": position === "first",
-                    }
-                )}
-            >
-                <UserAvatar
-                    user={worker}
-                    key={worker.id}
-                    className="border-accent-foreground border-1 shadow-2xs"
-                />
-                <span className="flex-1 text-nowrap hidden group-hover:block transition-all">
-                    {worker.firstname} {worker.lastname}
-                </span>
-            </Link>
-        </TooltipTrigger>
-        <TooltipContent>
-            {worker.firstname} {worker.lastname}
-        </TooltipContent>
-    </Tooltip>
+  <Link
+    href={Routes.USER(worker.id)}
+    className={cn(
+      "group flex items-center gap-x-2 hover:z-10 -mr-4 transition-all",
+      {
+        "hover:mr-0.5": position === "last",
+        "hover:px-2 hover:mr-2": position === "center",
+        "hover:pl-2.5 hover:pr-0.5": position === "first",
+      }
+    )}
+  >
+    <UserAvatar
+      user={worker}
+      key={worker.id}
+      className="border-accent-foreground border-1 shadow-2xs"
+    />
+    <span className="flex-1 text-nowrap hidden group-hover:block transition-all">
+      {worker.firstname} {worker.lastname}
+    </span>
+  </Link>
 );
-
-
-
-
-
-
-
--- src/entities/task/ui/task-page-card/parts/task-card-workers-list.tsx
+++ src/entities/task/ui/task-page-card/parts/task-card-workers-list.tsx
@@ -1,70 +1,61 @@
-import {TypeTaskWorker} from "@/entities/task/model/types/task";
-import {TaskCardWorker} from "./task-card-worker";
-import {CircleEllipsis} from "lucide-react";
-import {Tooltip, TooltipContent, TooltipTrigger} from "@/shared/ui";
+import { TypeTaskWorker } from "@/entities/task/model/types/task";
+import { TaskCardWorker } from "./task-card-worker";
+import { Ellipsis } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/shared/ui";
 import Link from "next/link";
-import {Routes} from "@/shared/routes/paths";
+import { Routes } from "@/shared/routes/paths";
 
 export const TaskCardWorkersList = ({
   workers,
 }: {
   workers?: TypeTaskWorker[];
 }) => {
-    if (!(workers?.length && workers.length > 0)) return (<p>Нет Работников</p>);
-    let workersInTooltip: TypeTaskWorker[] | undefined;
+  if (!(workers?.length && workers.length > 0)) return <p>Нет Работников</p>;
+  let workersInTooltip: TypeTaskWorker[] | undefined;
 
-    if (workers.length > 3) {
-        workersInTooltip = workers.slice(3, workers.length);
-        workers = workers.slice(0, 3);
-    }
+  if (workers.length > 3) {
+    workersInTooltip = workers.slice(3, workers.length);
+    workers = workers.slice(0, 3);
+  }
 
-    return (
-           <div className="flex items-center justify-end flex-row-reverse">
-               {workersInTooltip
-                   ? <Tooltip>
-                       <TooltipTrigger
-                           className='hover:ml-2.5 cursor-pointer'
-                           asChild>
-                               <CircleEllipsis
-                                   size='32'
-                               />
-                       </TooltipTrigger>
-                       <TooltipContent>
-                           <ol>
-                               {workersInTooltip.map((worker, index) => <li key={index}>
-                                   <Link
-                                       className='hover:underline'
-                                       href={Routes.USER(worker.id)}>
-                                       {index + 4}. {worker.firstname} {worker.lastname}
-                                   </Link>
-                               </li>)}
-                           </ol>
-                       </TooltipContent>
-                   </Tooltip>
-                   : null
-               }
-            {workers.map((worker, index, array) => (
-                <Tooltip key={worker.id}>
-                    <TooltipTrigger asChild>
-                        <Link href={Routes.USER(worker.id)}>
-                            <TaskCardWorker
-                                worker={worker}
-                                key={worker.id}
-                                position={
-                                    index === 0
-                                        ? "first"
-                                        : index === array.length - 1
-                                            ? "last"
-                                            : "center"
-                                }
-                            />
-                        </Link>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                        {worker.firstname} {worker.lastname}
-                    </TooltipContent>
-                </Tooltip>
-            ))}
-        </div>
-    );
-}
+  return (
+    <div className="flex gap-x-6">
+      <div className="flex items-center justify-end flex-row-reverse">
+        {workers.map((worker, index, array) => (
+          <TaskCardWorker
+            worker={worker}
+            key={worker.id}
+            position={
+              index === 0
+                ? "first"
+                : index === array.length - 1
+                ? "last"
+                : "center"
+            }
+          />
+        ))}
+      </div>
+      {workersInTooltip && workersInTooltip.length > 0 && (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Ellipsis className="size-8 border border-accent-foreground bg-muted rounded-full hover:p-0.5 hover:scale-115 duration-750 transition-[padding,transform]" />
+          </TooltipTrigger>
+          <TooltipContent>
+            <ol>
+              {workersInTooltip.map((worker, index) => (
+                <li key={index}>
+                  <Link
+                    className="hover:underline"
+                    href={Routes.USER(worker.id)}
+                  >
+                    {index + 4}. {worker.firstname} {worker.lastname}
+                  </Link>
+                </li>
+              ))}
+            </ol>
+          </TooltipContent>
+        </Tooltip>
+      )}
+    </div>
+  );
+};
